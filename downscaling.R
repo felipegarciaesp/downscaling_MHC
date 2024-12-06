0e384a30e5593fbe9c8515b369fb39bb6f805438
Felipe Garcia <felipegarcia.esp@gmail.com>
2024-12-06 11:27:09 -0300

    gitignore

--- downscaling.R
+++ downscaling.R
@@ -1,3 +1,17 @@
 # Se borran todas las variables y figuras.
 rm(list=ls())
 graphics.off()
+
+# =====================================================================
+# Codigo para Delta Change y Quantile Mapping / Felipe Garcia
+# =====================================================================
+
+# Carga de paquetes:
+library(openxlsx)
+
+# Se fija el directorio de trabajo:
+setwd("C:/Users/Usuario/Codigos_R/downscaling")
+
+# # Se identifica nombre de la carpeta a utilizar:
+carpeta <- "archivos"
+ruta_archivos <- paste0(getwd(),"/",carpeta)
